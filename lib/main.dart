7d3d8d0b0519ffdbdb011aad4da0269d1a826121
Eric Mengelkoch <134116393+EricMengelkoch@users.noreply.github.com>
2024-08-02 00:37:32 -0500

    Added loading and home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:melee_app/pages/home.dart';
+import 'package:melee_app/pages/loading.dart';
 
 void main() {
-  runApp();
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+    },
+  ));
 }
 
---
+++ lib/pages/loading.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void loadUser() async {
+    await Future.delayed(const Duration(seconds: 2), (){});
+    Navigator.pushReplacementNamed(context, '/home');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadUser();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[800],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "EVERYTHING MELEE",
+              style: TextStyle(
+                fontSize: 36.0,
+              ),
+              ),
+            Text("Loading..."),
+            SizedBox(height: 20.0),
+            SpinKitDualRing(
+              color: Colors.white,
+              size: 50.0,
+            ),
+        ],)
+      ),
+    );
+  }
+}
